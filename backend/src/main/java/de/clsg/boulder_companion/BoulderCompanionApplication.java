4bc9c5fe8e36997e64f5c37e16aedfbc4362636a
ChrisClsg <chris@clsg.de>
2026-04-27 14:11:51 +0200

    chore(backend): Init Spring Boot project
    Java 25
    Dep: Spring Web, Spring Data MongoDB, Spring OAuth2 Client

---
+++ backend/src/main/java/de/clsg/boulder_companion/BoulderCompanionApplication.java
@@ -0,0 +1,13 @@
+package de.clsg.boulder_companion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoulderCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoulderCompanionApplication.class, args);
+	}
+
+}
